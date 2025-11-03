2aee70bae0cee9b1dee9e65152a546010041aa19
Simon La Grange <sla-gran@shi-r9-p5.s19.be>
2025-11-03 15:16:26 +0100

    amelioration printf

--- ft_printf.c
+++ ft_printf.c
@@ -15,37 +15,42 @@
 int	ft_printf(const char *s, ...)
 {
 	va_list args;
+	size_t	i;
 
 	va_start(args, s);
+	i = 0;
 	while (!s[i])
 	{
 		if (s[i] == '%')
 		{
 			i++;
-			if (s[i] == 'c')
-				ft_putchar_fd((char)va_arg(args, char), 1);
-			else if (s[i] == 'd')
-				ft_putnbr_fd((int)va_arg(args, int), 1);
-			else if (s[i] == 's')
-				ft_putstr_fd((char *)va_arg(args, char*), 1);
-			else if (s[i] == 'u')
-				break;
-			else if (s[i] == 'i')	
-				ft_putnbr_fd((int)va_arg(args, int), 1);
-			else if (s[i] == 'x')
-				break;
-			else if (s[i] == 'X')
-				break;
-			else if (s[i] == '%')
-				ft_putchar_fd('%', 1);
-			else if (s[i] == 'p')
-				break;
+			if_cond(s, i, args);
 		}
 		else
-		{
 			ft_putchar_fd(*p, 1);
-		}
 		i++;
 	}
 	va_end(args);
+}
+
+void	if_cond(const char *s, size_t  i, va_list args)
+{
+	if (s[i] == 'c')
+		ft_putchar_fd((char)va_arg(args, char), 1);
+		else if (s[i] == 'd')
+			ft_putnbr_fd((int)va_arg(args, int), 1);
+		else if (s[i] == 's')
+			ft_putstr_fd((char *)va_arg(args, char*), 1);
+		else if (s[i] == 'u')
+			break; //unsigned int
+		else if (s[i] == 'i')	
+			ft_putnbr_fd((int)va_arg(args, int), 1);
+		else if (s[i] == 'x')
+			break; //hexadecimal => putnbr_base atoi_base a faire
+		else if (s[i] == 'X')
+			break; //hexadecimal => putnbr_base ou atoi_base a faire
+		else if (s[i] == '%')
+			ft_putchar_fd('%', 1);
+		else if (s[i] == 'p')
+			break; //print pointeur void
 }
